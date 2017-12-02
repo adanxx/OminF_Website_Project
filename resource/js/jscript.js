d546c40004d9a81cc0a0ab1b758cf01f7856eafb
adanxx <abdi0361@stud.kea.dk>
2017-12-02 18:09:31 +0100

    Add Scroll Navigation -Done

--- resource/js/jscript.js
+++ resource/js/jscript.js
@@ -9,20 +9,65 @@ $(document).ready(function(){
           $('nav').removeClass('sticky');
       }
   },{
-        offset: '60px;';
+        offset: '60px;'
     });
+    
+    /* button Scroll animation to the meal price-plan-section */
+    $('.js--scroll-meals').click(function(){
+        $('html, body').animate({scrollTop: $('.js--meal-plan').offset().top},1000);s
+    });
+    
+     /*button Scroll animation to the meal features-section*/
+    $('.js--scroll-show-more').click(function(){
+        $('html, body').animate({scrollTop: $('.js--section-features').offset().top},1000);
+    });
+    
+     /* Navigation scroll */
+    
+    /*
+    $(function() {
+      $('a[href*=#]:not([href=#])').click(function() {
+        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
+          var target = $(this.hash);
+          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+          if (target.length) {
+            $('html,body').animate({
+              scrollTop: target.offset().top
+            }, 1000);
+            return false;
+          }
+        }
+      });
+    });
+    */
+        $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        }
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
     
 });
                   
-/*
-
- var waypoint = new Waypoint({
-  element: document.getElementById('px-offset-waypoint'),
-  handler: function(direction) {
-    notify('I am 20px from the top of the window')
-  },
-  offset: '25' 
- })
-                  
-});
-*/
